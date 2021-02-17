4b568a8baf718b6d6d773e41d323544a62d8bdd8
colo1211 <colo1211@naver.com>
2021-02-17 14:11:28 +0900

    함수를 활용한 코드 리팩토링

--- curri1. Node.JS/main.js
+++ curri1. Node.JS/main.js
@@ -8,71 +8,29 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url,true).query; // id:'HTML', id:'CSS' , id:'JavaScript' 등과 같이 나온다(객체형식)
     var pathname = url.parse(_url,true).pathname; // pathname = '/', path='/?id=HTML
 
-    // console.log(url.parse(_url,true));
-
     if (pathname==='/') {
         if (queryData.id === undefined){ // main 페이지
-            var title = 'WelCome';
-            var description= 'Hello Node.js';
-
             // 메인페이지에서 리스트 처리
             fs.readdir('./data',function(error,fileName){
-                var list= `<ul>`;
-                var i =0;
-                while(i<fileName.length){
-                    list = list+`<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
-                    i=i+1;
-                }
-                list = list+'</ul>';
-
-                var template=`<!doctype html>
-                    <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>`;
-                        response.writeHead(200); // 성공적으로 로딩
-                        response.end(template); // 어떤 코드를 넣느냐에 따라서, 사용자에게 전송하는 데이터가 바뀐다.
-            })
-        }else { // pathname이 undefined가 아니라면 ex) CSS,HTML 등
-            var title = queryData.id;
-
-            // 메인 페이지가 아닌 곳에서의 리스트 처리
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (error, description) {
-                fs.readdir('./data',function(error,fileName){
-                    var list= `<ul>`;
-                    var i =0;
-                    while(i<fileName.length){
-                        list = list+`<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
-                        i=i+1;
-                    }
-                    list = list+'</ul>';
-
-                    var template = `
-                        <!doctype html>
-                        <html>
-                        <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                        </head>
-                        <body>
-                        <h1><a href="/?id=Web">WEB</a></h1>
-                        ${list}    
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                        </body>
-                        </html>`;
+                var title = 'WelCome';
+                var description= 'Hello Node.js';
+                var list = listMaker(fileName);
+                var template=templateTxt(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200); // 성공적으로 로딩
                 response.end(template); // 어떤 코드를 넣느냐에 따라서, 사용자에게 전송하는 데이터가 바뀐다.
             })
-        })
+        }else { // pathname이 undefined가 아니라면 ex) CSS,HTML 등
+            // 메인 페이지가 아닌 곳에서의 리스트 처리
+                fs.readdir('./data',function(error,fileName){
+                    var list = listMaker(fileName);
+                    fs.readFile(`./data/${queryData.id}`,'utf8',function(error,description){
+                        var title = queryData.id;
+                        var template = templateTxt(title,list,`<h2>${title}</h2>${description}`);
+                        response.writeHead(200); // 성공적으로 로딩
+                        response.end(template); // 어떤 코드를 넣느냐에 따라서, 사용자에게 전송하는 데이터가 바뀐다.
+                    })
+            })
+
     }
     }
 
@@ -83,3 +41,30 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+
+function templateTxt(title, list, body){ // 화면에 출력할 template을 만들어 주는 함수
+    return `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/?id=Web">WEB</a></h1>
+        ${list}    
+        ${body};
+        </body>
+        </html>
+     `;
+}
+function listMaker(fileName){ // 파일 리스트를 만들어 주는 함수
+    var list= `<ul>`;
+    var i =0;
+    while(i<fileName.length){
+        list = list+`<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
+        i=i+1;
+    }
+    list = list+'</ul>';
+    return list;
+}
--- curri1. Node.JS/remain.js
+++ curri1. Node.JS/remain.js
@@ -16,76 +16,58 @@ var app = http.createServer(function(request,response){
             var title= 'Welcome';
             var description = 'Hello Node';
             fs.readdir('./data',function(error, fileName){
-
-                var list = `<ul>`;
-                var i=0;
-                while(i<fileName.length){
-                    list = list + `<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
-                    i++;
-                }
-                list = list+`</ul>`;
-                var template = `
-               <!doctype html>
-                <html>
-                <head>
-                  <title>${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}  
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-                </html>
-            `;
+                var list = listMaker(fileName);
+                var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>`);
                 response.writeHead(200);
                 response.end(template);
             })
         }else { // main 페이지가 아닌 다른 페이지
-            var title = queryString.id;
-
             // 실수한 부분 - readFile에서 파일위치만 명시하고 파일 명을 빼먹음
-            fs.readFile(`./data/${queryString.id}`,'utf8',function (error, description){
-                fs.readdir(`./data`,function (error,fileName){
-                    if (!fileName.includes(queryString.id)) {
-                        response.writeHead(404);
-                        response.end(`Not Found`);
-                    }
-                    else {
-                        var list = `<ul>`;
-                        var i = 0;
-                        while (i < fileName.length) {
-                            list = list + `<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
-                            i++;
-                        }
-                        list = list + `</ul>`;
-
-                        var template =
-                            `<!doctype html>
-                        <html>
-                        <head>
-                            <title>${title}</title>
-                            <meta charset="utf-8">
-                        </head>
-                        <body>
-                        <h1><a href="/">WEB</a></h1>
-                        ${list}
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                        </body>
-                        </html>`;
+            fs.readdir(`./data`, function (error, fileName) {
+                if (!fileName.includes(queryString.id)) {
+                    response.writeHead(404);
+                    response.end(`Not Found`);
+                } else {
+                    var list = listMaker(fileName);
+                    fs.readFile(`./data/${queryString.id}`, 'utf8', function (error, description) {
+                        var title = queryString.id;
+                        var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>` )
                         response.writeHead(200);
                         response.end(template)
-                    }
-                })
-
-            });
-
-            }
+                    })
+                }
+            })
+        }
     }else{ // 아예 이상한 URL
         response.writeHead(404);
         response.end('Not Found');
     }
 });
 app.listen(3000);
+
+function listMaker(fileName){
+    var list = `<ul>`;
+    var i = 0;
+    while (i < fileName.length) {
+        list = list + `<li><a href="/?id=${fileName[i]}">${fileName[i]}</a></li>`;
+        i++;
+    }
+    list = list + `</ul>`;
+    return list;
+}
+function templateMaker(title, list, body){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+        <title>${title}</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `
+}
