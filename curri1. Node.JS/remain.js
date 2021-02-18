2f38f4b54e6ee1de3ec928bb3488b1c003f3b009
colo1211 <colo1211@naver.com>
2021-02-18 20:18:38 +0900

    Post 방식으로 데이터를 전송할때, form 형식

--- curri1. Node.JS/remain.js
+++ curri1. Node.JS/remain.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs= require('querystring');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryString = url.parse(_url,true).query;
     var pathname = url.parse(_url,true).pathname;
+    // console.log(pathname);
 
     // query는 객체 형식으로 어떤 queryString 이 입력 되었는지 파악하기 위한 API
     // pathname : /으로 들어온다.
@@ -17,28 +19,71 @@ var app = http.createServer(function(request,response){
             var description = 'Hello Node';
             fs.readdir('./data',function(error, fileName){
                 var list = listMaker(fileName);
-                var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>`);
+                var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a>`
+            );
                 response.writeHead(200);
                 response.end(template);
             })
         }else { // main 페이지가 아닌 다른 페이지
-            // 실수한 부분 - readFile에서 파일위치만 명시하고 파일 명을 빼먹음
             fs.readdir(`./data`, function (error, fileName) {
-                if (!fileName.includes(queryString.id)) {
-                    response.writeHead(404);
-                    response.end(`Not Found`);
-                } else {
                     var list = listMaker(fileName);
                     fs.readFile(`./data/${queryString.id}`, 'utf8', function (error, description) {
                         var title = queryString.id;
-                        var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>` )
+                        var template = templateMaker(title, list,`<h2>${title}</h2><p>${description}</p>`,
+                            `    
+                                <a href="/create">create</a> 
+                                <a href="/update">update</a>` )
                         response.writeHead(200);
                         response.end(template)
                     })
-                }
             })
         }
-    }else{ // 아예 이상한 URL
+    }else if (pathname ==='/create') {
+        fs.readdir('./data',function(error , fileName){
+            var title = 'List-Create';
+            var list = listMaker(fileName);
+            var template = templateMaker(title, list, `
+                <form action="/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="description" placeholder="description"></textarea></p>
+                <p><input type="submit"></p>
+                </form>`);
+            response.writeHead(200);
+            response.end(template);
+        })
+    }else if (pathname==='/process_create'){
+        var body;
+        request.on('data',function(data){
+            body = body+data;
+        });
+        request.on('end',function (){
+            var post = qs.parse(body);
+            var title = post.undefinedtitle;
+            var description = post.description;
+            fs.writeFile(`data/${title}`,description,'utf8',function(error){
+                response.writeHead(302,{Location: `/?id=${title}`});
+                response.end();
+            })
+        });
+    }
+    else if (pathname === '/update'){
+        fs.readFile(`data/${queryString.id}`,`utf8`,function (error,description){
+            fs.readdir('./data',function(error , fileName){
+                var title = queryString.id;
+                var list = listMaker(fileName);
+                var template = templateMaker(title, list, `
+                <form action="/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title" value=${title}></p>
+                <p><textarea name="description" placeholder="description">${description}</textarea></p>
+                <p><input type="submit"></p>
+                </form>`);
+                response.writeHead(200);
+                response.end(template);
+            })
+        })
+    }
+
+    else{ // 아예 이상한 URL
         response.writeHead(404);
         response.end('Not Found');
     }
@@ -55,7 +100,7 @@ function listMaker(fileName){
     list = list + `</ul>`;
     return list;
 }
-function templateMaker(title, list, body){
+function templateMaker(title, list, body,control){
     return `
     <!doctype html>
     <html>
@@ -66,6 +111,9 @@ function templateMaker(title, list, body){
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    ${control}
+<!--    <a href="/create">create</a> -->
+<!--    <a href="/update">update</a>-->
     ${body}
     </body>
     </html>
