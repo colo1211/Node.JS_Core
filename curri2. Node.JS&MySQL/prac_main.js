a8f4da4bb33090be7143976c6d61fee02b09fdc6
colo1211 <colo1211@naver.com>
2021-03-01 23:29:47 +0900

    main_mysql 복습

--- curri2. Node.JS&MySQL/prac_main.js
+++ curri2. Node.JS&MySQL/prac_main.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 var db= mysql.createConnection({
     host: 'localhost',
     user: 'nodejs',
-    password: '**********',
+    password: '***********',
     database : 'opentutorials'
 });
 
